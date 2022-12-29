9c01d188629ab6fb9689e28d8f17adac103de590
tanducCS <99661719+tanducCS@users.noreply.github.com>
2022-12-30 01:25:55 +0700

    update product, account, payment,login,footer

---
+++ src/Account/script.js
@@ -0,0 +1,18 @@
+const img = document.querySelector('#photo');
+const file = document.querySelector('#file');
+const uploadBtn = document.querySelector('#uploadBtn');
+
+file.addEventListener('change', function(){
+
+    const choosedFile = this.files[0];
+    if (choosedFile){
+        const reader = new FileReader();
+        reader.addEventListener('load',function(){
+            img.setAttribute('src',reader.result)
+        });
+
+        reader.readAsDataURL(choosedFile);
+    }
+
+
+});
